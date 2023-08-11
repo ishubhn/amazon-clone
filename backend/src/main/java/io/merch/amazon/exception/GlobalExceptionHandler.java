4e67f9baaf8e98413c3f7181513a9a897996589d
Shubham Nagre <imshubhn@gmail.com>
2023-08-11 12:34:18 +0530

    Added global exception handler

--- backend/src/main/java/io/merch/amazon/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/io/merch/amazon/exception/GlobalExceptionHandler.java
@@ -1,4 +1,30 @@
 package io.merch.amazon.exception;
 
+import io.merch.amazon.models.dto.Status;
+import io.merch.amazon.models.dto.response.MessageResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+	@ExceptionHandler(InvalidArgumentException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public MessageResponse handleInvalidArgumentException(InvalidArgumentException ex, WebRequest request) {
+		return new MessageResponse("FAILURE", ex.getMessage(), request.getDescription(true));
+	}
+
+	@ExceptionHandler(InvalidAgeException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public MessageResponse handleInvalidAgeException(InvalidAgeException ex, WebRequest request) {
+		return new MessageResponse("FAILURE", ex.getMessage(), request.getDescription(true));
+	}
+
+	@ExceptionHandler(NoSuchUserExistException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public MessageResponse handleNoSuchUserExistException(NoSuchUserExistException ex, WebRequest request) {
+		return new MessageResponse("FAILURE", ex.getMessage(), request.getDescription(true));
+	}
 }
---
+++ backend/src/main/java/io/merch/amazon/exception/InvalidArgumentException.java
@@ -0,0 +1,7 @@
+package io.merch.amazon.exception;
+
+public class InvalidArgumentException extends RuntimeException {
+	public InvalidArgumentException(String s) {
+		super(s);
+	}
+}
