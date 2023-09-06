f176c315e5159d66bcdd106a8dfed2a04dfc33d3
Shubham Nagre <imshubhn@gmail.com>
2023-09-06 18:16:16 +0530

    nav 2 complete

--- ui/amazon-ui/src/app/dashboard/section/navbar-bottom/navbar-bottom.component.ts
+++ ui/amazon-ui/src/app/dashboard/section/navbar-bottom/navbar-bottom.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar-bottom',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar-bottom.component.css']
 })
 export class NavbarBottomComponent {
-
+  faCaretDown = faCaretDown;
 }
