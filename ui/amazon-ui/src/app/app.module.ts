1dee7f65cd438e644b1f621911b6c467de7cc940
Shubham Nagre <imshubhn@gmail.com>
2025-09-28 19:37:35 +0530

    upgrading to 18

--- ui/amazon-ui/src/app/app.module.ts
+++ ui/amazon-ui/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,19 +11,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { DashboardRoutingModule } from './dashboard/dashboard-routing.module';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AuthenticationModule,
-    AuthenticationRoutingModule,
-    DashboardModule,
-    DashboardRoutingModule,
-    HttpClientModule,
-    FontAwesomeModule,
-  ],
-  providers: [Title],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        AuthenticationModule,
+        AuthenticationRoutingModule,
+        DashboardModule,
+        DashboardRoutingModule,
+        FontAwesomeModule], providers: [Title, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
