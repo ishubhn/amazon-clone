f76a24d90295d69882029c7bf64343fded4785d5
Shubham Nagre <imshubhn@gmail.com>
2025-10-06 10:19:54 +0530

    Add CatalogueCard component with styles and unit tests

---
+++ ui/amazon-ui/src/app/shared/catalogue-card/catalogue-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CatalogueCardComponent } from './catalogue-card.component';
+
+describe('CatalogueCardComponent', () => {
+  let component: CatalogueCardComponent;
+  let fixture: ComponentFixture<CatalogueCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CatalogueCardComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CatalogueCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
